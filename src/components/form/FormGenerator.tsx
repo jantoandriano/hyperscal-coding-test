8c71ed809ad4249539a226df2c6e12b39eae92f9
jantoandriano <motulojanto@gmail.com>
2024-07-02 13:48:28 +0700

    fix: issue mentioned by browser console desc: fix issue that is throwing by browser console

--- src/components/form/FormGenerator.tsx
+++ src/components/form/FormGenerator.tsx
@@ -1,36 +1,37 @@
 'use client';
 import React from 'react';
 import {
-    FormGeneratorProps,
+   FormGeneratorProps,
 } from "@/components/form/types";
 import {FormGeneratorLoader} from "@/components/form/FormGeneratorLoader";
 import {FormField} from "@/components/form/FormFields";
 
 
 const FormGenerator
-    = React.forwardRef<HTMLFormElement, FormGeneratorProps>
+   = React.forwardRef<HTMLFormElement, FormGeneratorProps>
 (({schema, state: formGenState,
-      model, updateModelValue}) => {
-    if (formGenState.isLoading) {
-        return (<FormGeneratorLoader />)
-    }
+     model, updateModelValue}, ref) => {
+   if (formGenState.isLoading) {
+       return (<FormGeneratorLoader />)
+   }
 
-    // TODO: Challenge #5 last_name re-rendered as I change value for first_name. Figure how to optimize this.
-    return (
-        <>
-            {
-                schema.definitions.map((field) => (
-                    <FormField
-                        field={field}
-                        path={field.name} // if you are wondering why
-                        value={model?.[field.name]}
-                        errors={formGenState?.errors?.[field.name]}
-                        updateModelValue={updateModelValue}
-                    />
-                ))
-            }
-        </>
-    );
+   // TODO: Challenge #5 last_name re-rendered as I change value for first_name. Figure how to optimize this.
+   return (
+       <>
+           {
+               schema.definitions.map((field) => (
+                   <FormField
+                       key={field.name}
+                       field={field}
+                       path={field.name} // if you are wondering why
+                       value={model?.[field.name]}
+                       errors={formGenState?.errors?.[field.name]}
+                       updateModelValue={updateModelValue}
+                   />
+               ))
+           }
+       </>
+   );
 });
 
 FormGenerator.displayName = 'FormGenerator';
