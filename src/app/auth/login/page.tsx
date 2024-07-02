708bd0628de35194a0e41a5dcc54ca0f6a10cf01
jantoandriano <motulojanto@gmail.com>
2024-07-02 14:13:07 +0700

    Optional challenge #1 Desc: Use tailwind css to style login page and form page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,8 +5,7 @@ import { useFormGen } from '@/components/form/useFormGen';
 import { Button } from '@/components/ui/button';
 import { useRouter, useSearchParams } from 'next/navigation';
 export default function LoginPage() {
-    const router = useRouter();
-    const searchParams = useSearchParams()
+
 
     const schema = {
         name: "simple-form",
@@ -26,6 +25,7 @@ export default function LoginPage() {
         ],
     } as FormSchema;
 
+
     const {
         state,
         model,
@@ -34,16 +34,21 @@ export default function LoginPage() {
     }
         = useFormGen({
             schema: schema,
-            model: { first_name: "", last_name: "" }
+            model: { email: "", password: "" }
         });
 
-    const handleLogin = async (data: FormModel) => {
+
+    const router = useRouter();
+    const searchParams = useSearchParams()
+
+
+    const handleLogin = async (model: FormModel) => {
         const res = await fetch('/api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(model)
         });
         if (res.status === 200) {
             let returnUrl = searchParams.get('return');
@@ -56,11 +61,25 @@ export default function LoginPage() {
     // TODO: Challenge: #4 - Change to use form generator with useFormGenerator hook and do the submit
     // TODO: Optional Challenge #1 - Use tailwindcss to style the login page
     return (
-        <div className="w-full md:w-96">
-            <form onSubmit={handleSubmit(handleLogin)}>
-                <FormGenerator schema={schema} state={state} model={model} updateModelValue={updateModelValue} />
-                <Button type={'submit'} className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600 mt-4">Login</Button>
-            </form>
-        </div>
+        <>
+            <div className="py-16">
+                <div className="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
+                    <img className="hidden lg:block lg:w-1/2 bg-cover" src="https://images.unsplash.com/photo-1546514714-df0ccc50d7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80"
+                    />
+                    <div className="w-full p-8 lg:w-1/2">
+                        <h2 className="text-2xl font-semibold text-gray-700 text-center uppercase">Welcome</h2>
+                        <form onSubmit={handleSubmit(handleLogin)}>
+                            <FormGenerator schema={schema} state={state} model={model} updateModelValue={updateModelValue} />
+                            <Button type={'submit'} className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600 mt-4">Login</Button>
+                        </form>
+                        <div className="mt-4 flex items-center justify-between">
+                            <span className="border-b w-1/5 md:w-1/4"></span>
+                            <a href="#" className="text-xs text-gray-500 uppercase">or sign up</a>
+                            <span className="border-b w-1/5 md:w-1/4"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,9 +1,9 @@
 'use client';
-import {logout} from "@/actions/logout";
-import {FormModel, FormSchema} from "@/components/form/types";
-import {useFormGen} from "@/components/form/useFormGen";
-import {FormGenerator} from "@/components/form/FormGenerator";
-import {Button} from "@/components/ui/button";
+import { logout } from "@/actions/logout";
+import { FormModel, FormSchema } from "@/components/form/types";
+import { useFormGen } from "@/components/form/useFormGen";
+import { FormGenerator } from "@/components/form/FormGenerator";
+import { Button } from "@/components/ui/button";
 
 export default function FormPage() {
     const schema = {
@@ -12,14 +12,14 @@ export default function FormPage() {
             {
                 name: "first_name",
                 type: "text",
-                label: {text: "First Name"},
-                rules: [{name: "required"}]
+                label: { text: "First Name" },
+                rules: [{ name: "required" }]
             },
             {
                 name: "last_name",
                 type: "text",
-                label: {text: "Last Name"},
-                rules: [{name: "required"}]
+                label: { text: "Last Name" },
+                rules: [{ name: "required" }]
             },
         ],
     } as FormSchema;
@@ -31,9 +31,9 @@ export default function FormPage() {
         handleSubmit
     }
         = useFormGen({
-        schema: schema,
-        model: {first_name: "", last_name: ""}
-    });
+            schema: schema,
+            model: { first_name: "", last_name: "" }
+        });
 
     // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     //     e.preventDefault();
@@ -46,15 +46,27 @@ export default function FormPage() {
     // TODO: Challenge #2: Browser console is throwing a warning. Fix it.
     return (
         <>
-            <h1>Form Page</h1>
-            <form onSubmit={handleSubmit(logSubmit)}>
-                <FormGenerator schema={schema} state={state} model={model} updateModelValue={updateModelValue}/>
-                <Button type={"submit"}>Submit</Button>
-            </form>
-            <hr />
-            <form action={logout}>
-            <button type={"submit"}>Logout</button>
-            </form>
+            <div className="py-16">
+                <div className="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
+                    <img className="hidden lg:block lg:w-1/2 bg-cover" src="https://images.unsplash.com/photo-1546514714-df0ccc50d7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80"
+                    />
+                    <div className="w-full p-8 lg:w-1/2">
+                        <form onSubmit={handleSubmit(logSubmit)}>
+                            <FormGenerator schema={schema} state={state} model={model} updateModelValue={updateModelValue} />
+                            <Button type="submit" className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600 mt-4">Login</Button>
+                        </form>
+
+                        <form action={logout}>
+                            <Button className="bg-red-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-red-600 mt-4" type={"submit"}>Logout</Button>
+                        </form>
+                        <div className="mt-4 flex items-center justify-between">
+                            <span className="border-b w-1/5 md:w-1/4"></span>
+                            <a href="#" className="text-xs text-gray-500 uppercase">or sign up</a>
+                            <span className="border-b w-1/5 md:w-1/4"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
